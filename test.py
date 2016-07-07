f48ecc2c1fb2267b25b175072f2227393cfea9b2
Xujia Cao <xujiacao@Xujias-MacBook-Pro.local>
2016-07-06 23:46:30 -0700

    write a simple auto test, add some rule to ast

---
+++ test.py
@@ -0,0 +1,22 @@
+import os
+show_errors = True
+
+print "test starts"
+
+cmd = "make"
+print "running " + cmd + ":"
+os.system(cmd)
+
+print ""
+
+for index in range(24):
+    cmd = "./c99-- < tests/test" + str(index) +".c"
+    print "running " + cmd + ":"
+    os.system(cmd)
+
+
+cmd = "make clean"
+print "running " + cmd + ":"
+os.system(cmd)
+
+print ""
